3a4e30efb0a951766ce74c41704264c16d51b409
Eliot Fagnon <efagnon2002@gmail.com>
2022-11-04 15:38:02 +0100

    Initial commit

---
+++ src/main/java/com/eliotfgn/twittercloneapi/TwitterCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.eliotfgn.twittercloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneApiApplication.class, args);
+	}
+
+}
