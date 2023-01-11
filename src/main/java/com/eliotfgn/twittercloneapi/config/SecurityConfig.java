f857021198e4f403968c202cb8555dda5f70fa9b
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-11 22:49:40 +0100

    add spring security

---
+++ src/main/java/com/eliotfgn/twittercloneapi/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.eliotfgn.twittercloneapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
